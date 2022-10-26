a08e33e9a12365b9106f27cf083e793da7abc85a
mshari alsayari <32165999+MshariAlsayari@users.noreply.github.com>
2022-10-26 15:32:30 +0300

    - theming

--- app/src/main/java/com/msharialsayari/musrofaty/ui/toolbar/CollabsedToolbar.kt
+++ app/src/main/java/com/msharialsayari/musrofaty/ui/toolbar/CollabsedToolbar.kt
@@ -10,63 +10,7 @@ import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
 
 
-private val ContentPadding = 8.dp
 private val Elevation = 2.dp
-
-private const val Alpha = 0.75f
-
-private val ExpandedPadding = 1.dp
-private val CollapsedPadding = 3.dp
-
-private val ExpandedCostaRicaHeight = 20.dp
-private val CollapsedCostaRicaHeight = 16.dp
-
-private val ExpandedWildlifeHeight = 32.dp
-private val CollapsedWildlifeHeight = 24.dp
-
-private val MapHeight = CollapsedCostaRicaHeight * 2
-
-//@Preview
-//@Composable
-//fun CollapsingToolbarCollapsedPreview() {
-//    MusrofatyComposeTheme {
-//        CollapsingToolbar(
-//            title = "Sender",
-//            progress = 0f,
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .height(80.dp)
-//        )
-//    }
-//}
-//
-//@Preview
-//@Composable
-//fun CollapsingToolbarHalfwayPreview() {
-//    MusrofatyComposeTheme {
-//        CollapsingToolbar(
-//            title = "Sender",
-//            progress = 0.5f,
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .height(120.dp)
-//        )
-//    }
-//}
-//
-//@Preview
-//@Composable
-//fun CollapsingToolbarExpandedPreview() {
-//    MusrofatyComposeTheme {
-//        CollapsingToolbar(
-//            title = "Sender",
-//            progress = 1f,
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .height(160.dp)
-//        )
-//    }
-//}
 
 
 @Composable
@@ -152,38 +96,3 @@ private fun CollapsedToolbarCompose(
 
 }
 
-
-
-
-@Composable
-private fun CollapsingToolbarLayout(
-    modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
-) {
-    Layout(
-        modifier = modifier,
-        content = content
-    ) { measurables, constraints ->
-
-
-        val placeables = measurables.map {
-            it.measure(constraints)
-        }
-        layout(
-            width = constraints.maxWidth,
-            height = constraints.maxHeight
-        ) {
-
-            val expandedHorizontalGuideline = (constraints.maxHeight * 0.4f).roundToInt()
-            val collapsedHorizontalGuideline = (constraints.maxHeight * 0.5f).roundToInt()
-
-            val countryMap = placeables[0]
-
-            countryMap.placeRelative(
-                x = 0,
-                y = collapsedHorizontalGuideline - countryMap.height / 2,
-            )
-
-        }
-    }
-}
