acee42a9f5c59c7a1633a13716c2cbbef8b44047
Mshari <32165999+MshariAlsayari@users.noreply.github.com>
2024-01-30 13:05:15 +0300

    - building own backend

--- app/src/main/java/com/msharialsayari/musrofaty/business_layer/ErrorResponse.kt
+++ app/src/main/java/com/msharialsayari/musrofaty/business_layer/ErrorResponse.kt
@@ -3,8 +3,8 @@ package com.msharialsayari.musrofaty.business_layer
 import com.google.gson.annotations.SerializedName
 
 data class ErrorResponse(
-    @SerializedName("status")
+    @SerializedName("code")
     val errorCode: Int? = null,
-    @SerializedName("title")
+    @SerializedName("message")
     val errorMessage: String? = null,
 )
